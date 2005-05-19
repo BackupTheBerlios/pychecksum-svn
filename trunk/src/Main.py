97731e4f98245eb84aa66450cba4a68e4cb295d0
borco <borco@f43c5e71-d2f3-0310-8969-b2cd67a60173>
2005-05-19 05:24:31 +0000

    Added pychecksum.py and moved the main() from Main.py to pychecksum.py. Updated .todo. Added doc directory.

--- trunk/src/Main.py
+++ trunk/src/Main.py
@@ -1,25 +1,21 @@
 #! /usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
-GLADE_DIR = ""
-GLADE_FILE = "PyCheckSum.glade"
 INTERFACE_GNOME = "gnome"
 INTERFACE_GTK = "gtk"
 ALGO_MD5 = "md5"
 ALGO_SFV = "sfv"
 
+GLADE_FILE = "PyCheckSum.glade"
+
 import gtk
 import pango
 from gobject import idle_add as idle_add
 import gtk.glade
-import os.path
-from optparse import OptionParser
-import sys
 from xml.dom.minidom import parse as dom_parse
-
+import os.path
+from os.path import getsize as filesize
 from SumFile import *
-
-filesize = os.path.getsize
 
 def get_elements_by_attribute(node, name, d):
 	elements = []
@@ -34,10 +30,9 @@ def get_elements_by_attribute(node, name, d):
 				elements.append(c)
 	return elements
 
-def set_glade_icon(node):
+def set_glade_icon(glade_path, node):
 	try:
 		xml_icon = get_elements_by_attribute(node, "property", {"name":"icon"})[0]
-		glade_path = os.path.join(GLADE_DIR, GLADE_FILE)
 		file_name = os.path.join(os.path.dirname(glade_path), xml_icon.childNodes[0].nodeValue)
 		xml_icon.childNodes[0].nodeValue = file_name
 		return file_name
@@ -45,19 +40,19 @@ def set_glade_icon(node):
 		return ""
 	
 class BaseWindow(object):
-	def __init__(self, expanded, interface):
-		xml = self._read_glade(interface)
+	def __init__(self, expanded, interface, glade_dir = ""):
+		xml = self._read_glade(interface, glade_dir)
 		self.export_glade(xml, expanded)
 		
-	def _read_glade(self, interface):
-		glade_path = os.path.join(GLADE_DIR, GLADE_FILE)
+	def _read_glade(self, interface, glade_dir):
+		glade_path = os.path.join(glade_dir, GLADE_FILE)
 		if interface == INTERFACE_GNOME:
 			try:
 				xml_doc = dom_parse(glade_path)
 				xml_glade_interface = xml_doc.getElementsByTagName("glade-interface")[0]
 				xml_gnome_main = get_elements_by_attribute(xml_glade_interface, "widget", {"class":"GnomeApp"})[0]
 				
-				set_glade_icon(xml_gnome_main)
+				set_glade_icon(glade_path, xml_gnome_main)
 				
 				xml_main = get_elements_by_attribute(xml_glade_interface, "widget", {"class":"GtkWindow", "id":"main"})[0]
 				xml_vbox = get_elements_by_attribute(xml_main.getElementsByTagName("child")[0], "widget", {"class":"GtkVBox", "id":"vbox"})[0]
@@ -85,7 +80,7 @@ class BaseWindow(object):
 			xml_glade_interface.removeChild(get_elements_by_attribute(xml_glade_interface, "requires", {"lib":"gnome"})[0])
 			xml_glade_interface.removeChild(get_elements_by_attribute(xml_glade_interface, "requires", {"lib":"bonobo"})[0])
 			
-			set_glade_icon(get_elements_by_attribute(xml_glade_interface, "widget", {"class":"GtkWindow", "id":"main"})[0])
+			set_glade_icon(glade_path, get_elements_by_attribute(xml_glade_interface, "widget", {"class":"GtkWindow", "id":"main"})[0])
 			
 			xml_buf = xml_doc.toxml()
 			xml = gtk.glade.xml_new_from_buffer(xml_buf, len(xml_buf), 'main')
@@ -318,130 +313,3 @@ class VerifyWindow(BaseWindow):
 		self.frame_filter.set_sensitive(True)
 		self.sumfile = sumfile
 		yield False
-		
-def main():
-	platform_win32 = sys.platform.startswith("win")
-
-	if platform_win32:
-		usage = """
-\t%prog [-xv] [--md5|--sfv] -cFILE
-\t%prog [-xv] [--md5|--sfv] [-oFILE] [-bPATH] file1 [file2] [-i PATH1]
-\t%prog --register
-\t%prog --unregister
-\t%prog (-h|--help)"""
-	else:
-		usage = '%prog [-xvg] [--md5|--sfv] (-cFILE | [-oFILE] [-bPATH] file1 [file2] [-i PATH1])'
-	parser = OptionParser(usage = usage)
-
-	# the interface
-	# on windows we can't have gnome so we disable all interfac options!
-	if not platform_win32:
-		parser.add_option("-g", "--gnome", action = "store_const", 
-			const = INTERFACE_GNOME, dest="interface", help="use the gnome interface")
-	parser.set_defaults(interface = INTERFACE_GTK)
-
-	# on windows, we need to (un)register with the registry
-	if platform_win32:
-		parser.add_option("", "--register", action = "store_true", 
-			dest="register", help="register with the REGISTRY")
-		parser.add_option("", "--unregister", action = "store_true", 
-			dest="unregister", help="unregister from the REGISTRY")
-		
-	parser.add_option("", "--md5", action = "store_const", 
-		const = ALGO_MD5, dest="algo", help="check or create MD5's")
-	parser.add_option("", "--sfv", action = "store_const", 
-		const = ALGO_SFV, dest="algo", help="check or create SFV's")
-	parser.set_defaults(algo = ALGO_MD5)
-	
-	parser.add_option("-i", dest="ignore_dirs", action = "append",
-		help="ignore given dir", metavar="DIR", default = [])
-	parser.add_option("-x", action = "store_true", dest="expanded",
-		help="expand the details on stat-up", default = False)
-	parser.add_option("-v", action = "store_true", dest="verbose",
-		help="print status on the console", default = False)
-	parser.add_option("-c", dest="infilename",
-		help="verify checksums stored in FILE", metavar="FILE")
-	parser.add_option("-o", dest="outfilename",
-		help="store checksums in FILE", metavar="FILE")
-	parser.add_option("-b", dest="basedir",
-		help="compute paths relative to PATH", metavar="PATH")
-
-	parser.add_option("-f", dest="singlefile",
-		help="compute PATH's MD5 and store it in PATH.md5", metavar="PATH")
-	parser.add_option("-d", dest="singledir",
-		help="compute PATH's MD5 and store it in PATH.md5", metavar="PATH")
-	
-	(options, args) = parser.parse_args()	
-
-	finished = False
-	
-	if platform_win32:
-		import Register
-		
-		# (un)register from registry
-		if options.register and options.unregister:
-			parser.error('please use only one of the --register or --unregister, but not both')
-
-		if options.register:
-			Register.register(os.path.abspath(__file__))
-			finished = True
-		elif options.unregister:
-			Register.unregister()
-			finished = True
-
-	if not finished:
-		if options.algo == ALGO_MD5:
-			sum = Md5File()
-			ext_output = ".md5"
-		else:
-			sum = SfvFile()
-			ext_output = ".sfv"
-			
-		if options.singlefile:
-			# create sum for a single file
-			basedir = os.path.dirname(options.singlefile)
-			outfilename = options.singlefile + ext_output
-			w = CreateWindow(options.expanded, options.interface)
-			w.show_all()
-			idle_add(w.create_sum(sum, [options.singlefile], outfilename, None, basedir, options.verbose).next)
-		
-		elif options.singledir:
-			# create sum for a single dir
-			basedir = '.'
-			options.singledir = options.singledir.rstrip("\\").rstrip("/").rstrip('"')
-			outfilename = os.path.join(options.singledir, os.path.basename(options.singledir)) + ext_output
-			try:
-				os.remove(outfilename)
-			except OSError:
-				pass
-			w = CreateWindow(options.expanded, options.interface)
-			w.show_all()
-			idle_add(w.create_sum(sum, [options.singledir], outfilename, options.ignore_dirs, basedir, options.verbose).next)
-		
-		else:
-			if len(args) == 0 and options.infilename == None:
-				parser.error('use -cFILE or provide a list of files')
-
-			# check files against an existing md5sum file
-			if options.infilename:
-				w = VerifyWindow(options.expanded, options.interface)
-				w.show_all()
-				idle_add(w.verify_sum(sum, options.infilename, options.verbose).next)
-			
-			else:
-				w = CreateWindow(options.expanded, options.interface)
-				w.show_all()
-				if options.outfilename:
-					try:
-						os.remove(options.outfilename)
-					except OSError:
-						pass
-				idle_add(w.create_sum(sum, args, options.outfilename, options.ignore_dirs, options.basedir, options.verbose).next)
-		gtk.main()
-	
-if __name__ == '__main__':
-	moduledir = os.path.abspath(os.path.dirname(__file__))
-	sys.path.append(moduledir)
-	GLADE_DIR = moduledir
-	main()
-
